609aa0dca4452ef948c135ee759535452c354cd2
akojm <ako.achats@free.fr>
2022-07-17 06:20:26 +0200

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburgerButton = document.querySelector(".nav-toggler")
+const navigation = document.querySelector("nav")
+const title = document.querySelector("h1")
+
+hamburgerButton.addEventListener("click", togglerNav)
+
+function togglerNav(){
+hamburgerButton.classList.toggle("active");
+navigation.classList.toggle("active");
+title.classList.toggle("active");
+}
